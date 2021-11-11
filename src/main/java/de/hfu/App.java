0b13e9ee4e65eead1c5c82bd0e985f2904ccc899
Your Name <matthias.tadeusz.leray@hs-furtwangen.de>
2021-11-11 12:38:44 +0100

    hi

---
+++ src/main/java/de/hfu/App.java
@@ -0,0 +1,12 @@
+package de.hfu;
+import java.util.*;
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Scanner a=new Scanner(System.in);
+        System.out.println( "Input:" );
+        String input=a.nextLine();
+        System.out.println(input.toUpperCase());
+    }
+}
